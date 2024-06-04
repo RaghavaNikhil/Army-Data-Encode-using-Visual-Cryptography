b67752dcd842782e6b6cc4d6ed467f6672b47b05
Moolaveesala Venkata Raghava Nikhil <mvr.nikhil@gmail.com>
2024-06-04 11:36:16 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='finalyear',
+    version='1.0',
+    packages=['finalyear', 'imageshare', 'imageshare.views', 'imageshare.migrations'],
+    url='',
+    license='',
+    author='1A',
+    author_email='mvraghavanikhil.16.cse@anits.edu.in',
+    description=''
+)

0b8ac7fd029858bf7072febfde325a63f43673b3
Moolaveesala Venkata Raghava Nikhil <mvr.nikhil@gmail.com>
2024-07-02 08:44:17 -0700

    Update computerVision.py

--- imageshare/views/computerVision.py
+++ imageshare/views/computerVision.py
@@ -2,11 +2,11 @@ import requests
 
 url = "https://joeschmoe.io/api/v1/shivam"
 
-querystring = {"eid":"1234%2F12345%2F12345","etimestamp":"11%2F11%2F2012 11%3A11%3A11","secretKey":"OBdz4sIn5fN9vgmrjaPU3wZ%2BO0mX9ndJNWCfM%2FjzeaAmc1qLSzaP7KfM4vnKJp44V0pbhajzOL2M82jJO%2FwwPmZetFO9vdsk8XJkOX7%2FfMHyoTI00FYbfdOJluk5UlNK","captchaValue":"1234"}
+querystring = {"eid":"1234%2F12345%2F12345","etimestamp":"11%2F11%2F2012 11%3A11%3A11","secretKey":<secretkey>,"captchaValue":"1234"}
 
 headers = {
     'x-rapidapi-host': "aadharcardstatus.p.rapidapi.com",
-    'x-rapidapi-key': "3af9d30ea9msh32e3cfdb7df1452p16af0ejsn9cb33927a52c"
+    'x-rapidapi-key': <key>
     }
 
 response = requests.request("GET", url)

9a3676a6cd348fbcd9b6b9d2d6e3f8be2d81f20e
Moolaveesala Venkata Raghava Nikhil <mvr.nikhil@gmail.com>
2024-07-02 08:52:09 -0700

    Update settings.py

--- finalyear/settings.py
+++ finalyear/settings.py
@@ -26,7 +26,7 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_j62!$s$-7l-6m%0q3!^#muhn-6-y*k2_0w_#u&4tcrlv)2xdd'
+SECRET_KEY = <secretkey>
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
